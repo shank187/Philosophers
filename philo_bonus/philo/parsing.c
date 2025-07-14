5ee11392d9ee262653d86a0d21125beb5e0a90dc
Aymane Elbour <aelbour@e3r4p5.1337.ma>
2025-07-14 14:46:32 +0100

    removed pthread_exit

---
+++ philo_bonus/philo/parsing.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelbour <aelbour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 14:30:25 by aelbour           #+#    #+#             */
+/*   Updated: 2025/07/13 10:55:44 by aelbour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_cleanup_table(t_data *data, int up_to)
+{
+	int	i;
+
+	i = -1;
+	while (++i < up_to)
+	{
+		pthread_detach(data->philos[i].thread);
+	}
+}
+
+void	ft_cleanup_forks(t_data *data, int up_to)
+{
+	int	i;
+
+	i = -1;
+	while (++i < up_to)
+	{
+		pthread_mutex_destroy(&data->forks[i]);
+	}
+}
+
+int	init_semp(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	while (++i < data->num_philos)
+		if (pthread_mutex_init(&(data->forks[i]), NULL))
+			return (ft_cleanup_forks(data, i), 0);
+	if (pthread_mutex_init(&(data->print_lock), NULL))
+		return (ft_cleanup_forks(data, i), 0);
+	if (pthread_mutex_init(&(data->death_lock), NULL))
+		return (pthread_mutex_destroy(&(data->print_lock)), \
+				ft_cleanup_forks(data, i), 0);
+	data->philos = malloc((data->num_philos) * sizeof(t_philo));
+	if (!data->philos)
+		return (pthread_mutex_destroy(&(data->death_lock)), \
+				pthread_mutex_destroy(&(data->print_lock)), \
+				ft_cleanup_forks(data, i), 0);
+	return (1);
+}
+
+int	init_philo_tools(t_data *data)
+{
+	int	i;
+
+	if (!init_semp(data))
+		return (0);
+	i = -1;
+	while (++i < data->num_philos)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].meals_eaten = 0;
+		data->philos[i].right_fork = &data->forks[i];
+		data->philos[i].left_fork = &data->forks[(i + 1) % data->num_philos];
+		data->philos[i].data = data;
+	}
+	return (1);
+}
+
+int	parse_init(int ac, char **av, t_data *data)
+{
+	int			error;
+
+	error = 0;
+	if (ac != 5 && ac != 6)
+		return (0);
+	data->num_philos = ft_atoi(av[1], &error);
+	data->time_to_die = ft_atoi(av[2], &error);
+	data->time_to_eat = ft_atoi(av[3], &error);
+	data->time_to_sleep = ft_atoi(av[4], &error);
+	data->meals_required = 0;
+	data->stop = 0;
+	data->crush = 0;
+	if (av[5])
+		data->meals_required = ft_atoi(av[5], &error);
+	if (error || data->num_philos > 200)
+		return (write(2, "parse error\n", 12), 0);
+	data->forks = malloc(data->num_philos * sizeof(pthread_mutex_t));
+	if (!data->forks)
+		return (write(2, "A FUNCTION FAILED !!\n", 21), 0);
+	if (!init_philo_tools(data))
+		return (write(2, "A FUNCTION FAILED !!\n", 21), free(data->forks), 0);
+	return (1);
+}
---
+++ philo_bonus/philo/philo_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelbour <aelbour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 15:40:43 by aelbour           #+#    #+#             */
+/*   Updated: 2025/07/13 11:25:06 by aelbour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	handle_crushes(t_data *data)
+{
+	free(data->forks);
+	free(data->philos);
+	free(data);
+	write(2, "A FUNCTION FAILED !!\n", 21);
+	exit(1);
+}
+
+int	safe_printf(char *str, t_philo *philo)
+{
+	if (are_u_alives(philo->data))
+	{
+		if (pthread_mutex_lock(&philo->data->print_lock))
+			handle_crushes(philo->data);
+		printf("%lld %i %s\n", philo_get_time() - \
+			philo->data->start_time, philo->id, str);
+		if (pthread_mutex_unlock(&philo->data->print_lock))
+			handle_crushes(philo->data);
+		return (1);
+	}
+	return (0);
+}
+
+int	are_u_alives(t_data *data)
+{
+	pthread_mutex_lock(&data->death_lock);
+	if (!data->stop)
+	{
+		if (pthread_mutex_unlock(&data->death_lock))
+			data->crush = 1;
+		return (1);
+	}
+	else
+	{
+		if (pthread_mutex_unlock(&data->death_lock))
+			data->crush = 1;
+		return (0);
+	}
+}
+
+long long	philo_get_time(void)
+{
+	struct timeval	tv;
+	long long		ms_time;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		return (-1);
+	ms_time = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	return (ms_time);
+}
+
+int	ft_usleep(size_t milliseconds, t_data *data)
+{
+	size_t	start;
+
+	start = philo_get_time();
+	while ((philo_get_time() - start) < milliseconds)
+	{
+		if (!are_u_alives(data))
+			return (0);
+		if (usleep(100))
+			data->crush = 1;
+	}
+	return (1);
+}
